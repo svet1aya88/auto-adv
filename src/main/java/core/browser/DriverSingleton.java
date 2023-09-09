7b529a7fcc7bbd82b6e8809c45655efcf9770b1c
Sviatlana_Hancharova <sviatlana_hancharova@epam.com>
2023-09-09 19:01:20 +0200

    initial commit

--- src/main/java/core/browser/DriverSingleton.java
+++ src/main/java/core/browser/DriverSingleton.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.NoSuchSessionException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
-import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverSingleton {
 
